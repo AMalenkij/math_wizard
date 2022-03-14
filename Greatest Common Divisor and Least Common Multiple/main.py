e1ffbcdd43f8fdbf0fcaac6fb2c43453fcf75412
Anton Malenkij <itottoro@gmail.com>
2022-03-15 00:58:00 +0100

    feat: add new func gcd

--- Greatest Common Divisor and Least Common Multiple/main.py
+++ Greatest Common Divisor and Least Common Multiple/main.py
@@ -1,3 +1,16 @@
+#greatest common division(GCD)
+count = []
+def gcd(a = 24):
+    if a != 1:
+        if a % 2 == 0:
+            count.append(2)
+            gcd(a=a/2)
+        else:
+            count.append(3)
+            gcd(a=a/3)
+
+print(gcd())
+print(count)
 
 if __name__ == '__main__':
 
