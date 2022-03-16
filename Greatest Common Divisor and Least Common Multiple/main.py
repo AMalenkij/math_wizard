397c7205ecb3ed64722b3f7fd6834763dff75e38
Anton Malenkij <itottoro@gmail.com>
2022-03-16 23:53:46 +0100

    refactor: func gcd test: add assert to func gcd

--- Greatest Common Divisor and Least Common Multiple/main.py
+++ Greatest Common Divisor and Least Common Multiple/main.py
@@ -1,16 +1,26 @@
-#greatest common division(GCD)
-count = []
-def gcd(a = 24):
-    if a != 1:
-        if a % 2 == 0:
-            count.append(2)
-            gcd(a=a/2)
-        else:
-            count.append(3)
-            gcd(a=a/3)
+# greatest common division(GCD)
+def gcd(a=34, b=12):
+    ans = 0
+    while True:
+        print('test')
+        if a > b:
+            ans = a - b
+        elif b > a:
+            ans = b - a
+        if ans > 0:
+            if a % ans == 0 and b % ans == 0:
+                return ans
+                break
 
-print(gcd())
-print(count)
+
+def multiply(lst):
+    answer = 1
+    for i in lst:
+        answer *= i
+    return answer
+
 
 if __name__ == '__main__':
-
+    assert gcd(36, 24) == 12
+    assert gcd(34, 12) == 2
+    assert gcd(15, 10) == 5
