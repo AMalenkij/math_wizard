ea35b1593f0b039ef6ee41ef5a3b1cc71c9abc4b
Anton Malenkij <itottoro@gmail.com>
2022-03-19 15:54:39 +0100

    refactor: gcd add args

--- Greatest Common Divisor and Least Common Multiple/main.py
+++ Greatest Common Divisor and Least Common Multiple/main.py
@@ -1,17 +1,18 @@
 # Greatest common divisor(GCD)
-def gcd(a: int, b: int) -> int:
+def gcd(*args: int) -> int:
     answer = []
-    copy_b = factor(b)
-    for t in factor(a):
-        for z in copy_b:
-            if t == z:
-                answer.append(t)
-                copy_b.pop(copy_b.index(z))
-                break
+    for g in args[1:]:
+        copy_b = factor(g)
+        for t in factor(args[0]):
+            for z in copy_b:
+                if t == z:
+                    answer.append(t)
+                    copy_b.pop(copy_b.index(z))
+                    break
     return multiply(answer)
 
 
-# Least common multiple
+# Least common multiple(LCM)
 def lcm(a: int, b: int) -> int:
     answer = []
     copy_b = factor(b)
@@ -61,3 +62,10 @@ if __name__ == '__main__':
     assert lcm(90, 20) == 180
     assert lcm(13, 11) == 143
     assert lcm(24, 32) == 96
+
+    assert gcd(36, 24, 12) == 12
+    assert gcd(34, 12, 4) == 2
+    assert gcd(15, 10, 5) == 5
+    assert gcd(90, 20, 11) == 10
+    assert gcd(13, 11, 2) == 1
+    assert gcd(24, 32, 16) == 8
